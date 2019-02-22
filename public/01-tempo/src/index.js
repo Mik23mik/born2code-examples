ff7422273bb32ea7c15be631c9542ad93636d28f
colthreepv <valerio.coltre@gmail.com>
2019-02-22 21:52:33 +0100

    utilizzo di una nuova firestore API: batch delete, ed uso dei DocumentReference

--- public/01-tempo/src/index.js
+++ public/01-tempo/src/index.js
@@ -2,6 +2,8 @@
 const awaitingEl = document.querySelector('#awaiting')
 const readyEl = document.querySelector('#ready')
 const timeEl = document.querySelector('#time')
+const cleanupEl = document.querySelector('#cleanup')
+const referencesEl = document.querySelector('#references')
 
 // invocato quando siamo pronti a visualizzare il primo orario
 function ready () {
@@ -14,38 +16,75 @@ function drawTime (time) {
   timeEl.textContent = time
 }
 
+function showCleanup () {
+  cleanupEl.classList.remove('invisible')
+  aspetta(3).then(() => cleanupEl.classList.add('invisible'))
+}
+
+function updateReferenceCounter (howMany) {
+  if (howMany === 0) {
+    referencesEl.classList.add('invisible')
+    return
+  }
+
+  referencesEl.classList.remove('invisible')
+  const placeholder = referencesEl.querySelector('.placeholder')
+  placeholder.textContent = howMany
+}
+
 document.addEventListener('DOMContentLoaded', init)
 
 function init () {
   const db = firebase.firestore()
-
   const orologio = db.collection('orologio')
+
+  // array di DocumentReference da rimuovere - altrimenti il database si riempirà inevitabilmente
+  const docRefToBeRemoved = []
 
   // attende 1 secondo per dare tempo all'utente di rendersi conto
   // che al caricamento della pagina non abbiamo l'informazione orario
-  aspetta1Sec().then(() =>
-    orologio.orderBy('time', 'desc').limit(1).get()
-      .then(results => {
-        if (results.size === 0) return
-        const orario = results.docs[0].get('orario')
-        return orario
-      })
-      .then(drawTime)
-      .then(setupListener)
-  )
+  aspetta(1)
+    .then(() => orologio.orderBy('time', 'desc').limit(1).get())
+    .then(results => {
+      if (results.size === 0) return
+      const orario = results.docs[0].get('orario')
+      return orario
+    })
+    .then(drawTime)
+    .then(() => setupListener(docRefToBeRemoved))
 }
 
-function setupListener () {
+function setupListener (docRefToBeRemoved) {
   const db = firebase.firestore()
   const orologio = db.collection('orologio')
 
   orologio.orderBy('time', 'desc').onSnapshot(results => {
     if (results.size === 0) return
-    const orario = results.docs[0].get('orario')
+    const newOrario = results.docs[0]
+    const orario = newOrario.get('orario')
+
+    docRefToBeRemoved.push(newOrario.ref)
+
+    // operazioni di disegno
     drawTime(orario)
+    updateReferenceCounter(docRefToBeRemoved.length)
+
+    // se ho collezionato 5 documenti, dall'apertura della pagina, li rimuovo
+    // altrimenti il database si riempirà inesorabilmente
+    if (docRefToBeRemoved.length > 5) cleanupReferences(docRefToBeRemoved)
   })
 }
 
-function aspetta1Sec () {
-  return new Promise(resolve => setTimeout(resolve, 1000))
+function aspetta (secondi) {
+  return new Promise(resolve => setTimeout(resolve, secondi * 1000))
+}
+
+function cleanupReferences (docRefList) {
+  const db = firebase.firestore()
+  const deleteBatch = db.batch()
+  docRefList.forEach(docRef => deleteBatch.delete(docRef))
+  docRefList.splice(0) // svuoto la lista che si riferisce ai documenti 'orologio'
+
+  return deleteBatch.commit()
+    .then(showCleanup)
 }
